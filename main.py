84a59d2dae15b5c96a5aeb40e00de32d8c5958e9
Anthony <49297058+SomeRangerOSRS@users.noreply.github.com>
2020-09-20 17:40:52 -0300

    Initial Commit of All Items
    Inital Commit - Has server vars, and random amount of money per game.

---
+++ main.py
@@ -0,0 +1,23 @@
+""" Created by Anthony Probert
+    Attempt #1 at an Adventure Game
+    Written in Python. """
+import random
+
+# start global vars
+# player starts with 20 hp at the beginning of the game.
+playerHP = 20
+# player stats the game with a random amount of $ between 1-25.
+playerMoney = random.randint(1, 25)
+# player starts the game with no reputation.
+playerRep = 0
+# end global vars
+
+# start opening screen of the game.
+print("Welcome to Olaf's Big Adventure.")
+print("")
+print("************")
+print("Olaf's Starting HP", playerHP)
+print("Olaf's Starting Money", playerMoney)
+print("Olaf's Starting Reputation", playerRep)
+print("************")
+print("")
