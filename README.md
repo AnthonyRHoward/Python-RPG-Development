be5aa1c28e6cd0d8198c5a4e1a000d238d2a07f1
Anthony <49297058+SomeRangerOSRS@users.noreply.github.com>
2020-09-20 23:08:12 -0300

    Various Updates
    Converted all of the Vars over to Settings.py, created a monsters bonus class, monstersstats() class, and added a postfightsstats() class,

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# Olafs Big Adventure
- Choose Your Own Adventure Game Staring Olaf. 
+# Text Based RPG.
+ Choose Your Own Adventure Game 
+
+# Hello from Anthony.
+This was created without the use of any video tutorials, templates, or guides. 
+I wanted to prove to myself that I could make this on my own and be able to
+learn the in's and out's of programming.
+
+There are ovbious mistakes that have been made in creating this, however,
+I plan on learning from those and progressing my knowledge moving forward.
+Although this is easy for most, this was a first attempt at creating 
+something that wasn't a simple "Hello World.". 
+
+The goal is to eventually convert this into a Discord Bot by importing
+and redesigning this to work with the Discord.Py framework. 
--- main.py
+++ main.py
@@ -1,122 +1,173 @@
 """ Created by Anthony Probert
     Attempt #1 at an Adventure Game
     Written in Python. """
+
 import random
-
-
-# start vars
-playerHP = 20
-playerMoney = 0
-playerRep = 0
-playerName = ""
-playerName = input("What is your name, My Lord?:")
-playerClass = ""
-enemyAttack = 0
-magicAttack = 0
-rangeAttack = 0
-meleeAttack = 0
-magicDefence = 0
-rangeDefence = 0
-meleeDefence = 0
-monstersKilled = 0
-playerInput = ""
-possibleMonsters = ["Rat", "Dog", "Dragon", "Human", "Shawman", "Druid", "Guard"]
-activeMonster = ""
-shop = ["Dagger 6 Gold", "Shield 10 Gold", "Cape 4 Gold", "Whip 25 Gold"]
-# end Vars
+import settings
 
 
 def currenttasks():
-    global playerInput
-    playerInput = ""
-    playerInput = input("What would you like to do?")
+    settings.playerInput = ""
+    settings.playerInput = input("What would you like to do?")
     playersaid()
 
 
+def monsterbonus():
+    if settings.monstersKilled == 10:
+        settings.playerHP += 10
+    elif settings.monstersKilled == 20:
+        settings.playerHP += 15
+    elif settings.monstersKilled == 30:
+        settings.playerHP += 20
+
+
+def monsterstats():
+    if settings.playerClass == "Range" and settings.activeMonster == "Rat":
+        settings.enemyAttack = random.randint(1, 2)
+        settings.playerMoney += random.randint(0, 1)
+    elif settings.playerClass == "Magic" and settings.activeMonster == "Rat":
+        settings.enemyAttack = random.randint(1, 2)
+        settings.playerMoney += random.randint(0, 1)
+    elif settings.playerClass == "Melee" and settings.activeMonster == "Rat":
+        settings.enemyAttack = random.randint(1, 2)
+        settings.playerMoney += random.randint(0, 1)
+    elif settings.playerClass == "Range" and settings.activeMonster == "Dog":
+        settings.enemyAttack = random.randint(1, 2)
+        settings.playerMoney += random.randint(0, 1)
+    elif settings.playerClass == "Magic" and settings.activeMonster == "Dog":
+        settings.enemyAttack = random.randint(1, 3)
+        settings.playerMoney += random.randint(0, 1)
+    elif settings.playerClass == "Melee" and settings.activeMonster == "Dog":
+        settings.enemyAttack = random.randint(1, 2)
+        settings.playerMoney += random.randint(0, 1)
+    elif settings.playerClass == "Range" and settings.activeMonster == "Dragon":
+        settings.enemyAttack = random.randint(1, 4)
+        settings.playerMoney += random.randint(0, 5)
+    elif settings.playerClass == "Magic" and settings.activeMonster == "Dragon":
+        settings.enemyAttack = random.randint(1, 6)
+        settings.playerMoney += random.randint(0, 5)
+    elif settings.playerClass == "Melee" and settings.activeMonster == "Dragon":
+        settings.enemyAttack = random.randint(1, 3)
+        settings.playerMoney += random.randint(0, 5)
+    elif settings.playerClass == "Range" and settings.activeMonster == "Human":
+        settings.enemyAttack = random.randint(1, 6)
+        settings.playerMoney += random.randint(0, 3)
+    elif settings.playerClass == "Magic" and settings.activeMonster == "Human":
+        settings.enemyAttack = random.randint(1, 3)
+        settings.playerMoney += random.randint(0, 3)
+    elif settings.playerClass == "Melee" and settings.activeMonster == "Human":
+        settings.enemyAttack = random.randint(1, 2)
+        settings.playerMoney += random.randint(0, 3)
+    elif settings.playerClass == "Range" and settings.activeMonster == "Druid":
+        settings.enemyAttack = random.randint(1, 2)
+        settings.playerMoney += random.randint(0, 2)
+    elif settings.playerClass == "Magic" and settings.activeMonster == "Druid":
+        settings.enemyAttack = random.randint(1, 3)
+        settings.playerMoney += random.randint(0, 2)
+    elif settings.playerClass == "Melee" and settings.activeMonster == "Druid":
+        settings.enemyAttack = random.randint(1, 5)
+        settings.playerMoney += random.randint(0, 2)
+    elif settings.playerClass == "Range" and settings.activeMonster == "Guard":
+        settings.enemyAttack = random.randint(1, 3)
+        settings.playerMoney += random.randint(0, 9)
+    elif settings.playerClass == "Magic" and settings.activeMonster == "Guard":
+        settings.enemyAttack = random.randint(1, 3)
+        settings.playerMoney += random.randint(0, 9)
+    elif settings.playerClass == "Melee" and settings.activeMonster == "Guard":
+        settings.enemyAttack = random.randint(1, 2)
+        settings.playerMoney += random.randint(0, 9)
+    else:
+        print("Error, I don't have that combination in my database.")
+
+
 def attack():
-    global enemyAttack, monstersKilled, playerHP, possibleMonsters, activeMonster
-    activeMonster = random.choice(possibleMonsters)
-    enemyAttack = random.randint(1, 5)
-    print("You lost", enemyAttack, "HP", "while killing a", activeMonster)
-    playerHP -= enemyAttack
-    if playerHP <= 0:
-        print("You died while killing", activeMonster)
+    settings.activeMonster = random.choice(settings.possibleMonsters)
+    monsterstats()
+    settings.playerHP -= settings.enemyAttack
+    print("You lost", settings.enemyAttack, "HP", "while killing a", settings.activeMonster)
+    monsterbonus()
+    if settings.playerHP <= 0:
+        print("You died while killing", settings.activeMonster)
         exit(0)
     else:
-        monstersKilled += 1
-        activeMonster = ""
+        settings.monstersKilled += 1
+        settings.activeMonster = ""
 
 
 def playerstats():
     print("")
+    print("Welcome to the Mighty Adventure of", settings.playerName)
     print("")
-    print("Welcome to the Mighty Adventure of", playerName)
+    print("************")
+    print("Health Points", settings.playerHP)
+    print("Gold Coins", settings.playerMoney)
+    print("Reputation", settings.playerRep)
+    print("************")
+    print("Magic Attack", settings.magicAttack)
+    print("Magic Defence", settings.magicDefence)
+    print("Range Attack", settings.rangeAttack)
+    print("Range Defence", settings.rangeDefence)
+    print("Melee Attack", settings.meleeAttack)
+    print("Melee Defence", settings.meleeDefence)
+    print("************")
+    print("Monsters Killed", settings.monstersKilled)
+    print("************")
+    print("")
+
+
+def postfightstats():
     print("")
     print("************")
-    print(playerName, "Health Points", playerHP)
-    print(playerName, "Gold Coins", playerMoney)
-    print(playerName, "Reputation", playerRep)
-    print("************")
-    print("Magic Attack", magicAttack)
-    print("Magic Defence", magicDefence)
-    print("Range Attack", rangeAttack)
-    print("Range Defence", rangeDefence)
-    print("Melee Attack", meleeAttack)
-    print("Melee Defence", meleeDefence)
-    print("************")
-    print("Monsters Killed", monstersKilled)
+    print("Health Points", settings.playerHP)
+    print("Gold Coins", settings.playerMoney)
+    print("Reputation", settings.playerRep)
+    print("Monsters Killed", settings.monstersKilled)
     print("************")
     print("")
 
 
 def playersaid():
-    global playerInput
-    if playerInput == "Attack":
+    if settings.playerInput == "Attack":
         attack()
+        postfightstats()
+        currenttasks()
+    elif settings.playerInput == "Stats":
         playerstats()
         currenttasks()
-    elif playerInput == "stats":
-        playerstats()
-        currenttasks()
-    elif playerInput == "shops":
-        for i in shop:
+    elif settings.playerInput == "Shops":
+        for i in settings.shop:
             print(i, end=" ")
             print("")
         currenttasks()
-    elif playerInput == "quit":
+    elif settings.playerInput == "quit":
         exit(0)
     else:
         currenttasks()
 
 
 def playerclass():
-    global playerClass, rangeAttack, rangeDefence, magicDefence, magicAttack, meleeDefence, meleeAttack, \
-        playerRep, playerMoney
-    if playerClass == "Range":
-        rangeAttack = random.randint(1, 10)
-        magicDefence = random.randint(1, 10)
-        playerRep += random.randint(1, 5)
-        playerMoney = random.randint(1, 25)
-    elif playerClass == "Magic":
-        magicAttack = random.randint(1, 25)
-        playerRep += random.randint(1, 3)
-        playerMoney = random.randint(1, 25)
-    elif playerClass == "Melee":
-        meleeAttack = random.randint(1, 10)
-        rangeDefence = random.randint(1, 5)
-        meleeDefence = random.randint(1, 5)
-        playerRep += random.randint(1, 7)
-        playerMoney = random.randint(1, 25)
+    if settings.playerClass == "Range":
+        settings.rangeAttack = random.randint(1, 10)
+        settings.magicDefence = random.randint(1, 10)
+        settings.playerRep += random.randint(1, 5)
+        settings.playerMoney = random.randint(1, 25)
+    elif settings.playerClass == "Magic":
+        settings.magicAttack = random.randint(1, 25)
+        settings.playerRep += random.randint(1, 3)
+        settings.playerMoney = random.randint(1, 25)
+    elif settings.playerClass == "Melee":
+        settings.meleeAttack = random.randint(1, 10)
+        settings.rangeDefence = random.randint(1, 5)
+        settings.meleeDefence = random.randint(1, 5)
+        settings.playerRep += random.randint(1, 7)
+        settings.playerMoney = random.randint(1, 25)
     else:
         print("No Class Selected")
-        playerClass = input("What is your style of choice? (Magic, Melee, Range):")
+        settings.playerClass = input("What is your style of choice? (Magic, Melee, Range):")
 
 
-playerClass = input("What is your style of choice? (Magic, Melee, Range):")
+settings.playerClass = input("What is your style of choice? (Magic, Melee, Range):")
 
 playerclass()
 playerstats()
 currenttasks()
-
-
-
